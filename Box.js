b3de22754d8328a7cc4853aec3d0a522403bbda3
shwethaGitHub-spec <62240096+shwethaGitHub-spec@users.noreply.github.com>
2020-04-06 19:11:50 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box{
+    constructor(x,y,w,h) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w; 
+        this.h = h;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(1);
+        stroke(255);
+        fill(127);
+        rect(0,0,this.w,this.h);
+        pop();
+      }
+}
